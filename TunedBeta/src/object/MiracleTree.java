160eead774ae61bcefddda85567016fd7bc09ed8
dcyclone <dcortes@udel.edu>
2016-12-08 05:09:16 -0500

    miracle tree comments

--- TunedBeta/src/object/MiracleTree.java
+++ TunedBeta/src/object/MiracleTree.java
@@ -11,6 +11,7 @@ import framework.ObjectId;
 import gfx.Images;
 
 public class MiracleTree extends GameObject{
+	//Attributes
 	public int hp;
 	private long timer0=System.currentTimeMillis();
 	private long timer1=System.currentTimeMillis();
@@ -19,24 +20,42 @@ public class MiracleTree extends GameObject{
 	int stage = 0;
 	Images images;
 
+	/**
+	 * Constructor that constructs a miracle tree object. This object can be used to stop the boss run-off in game 2
+	 * 
+	 * @param x - x position of miracle plant 
+	 * @param y - y position of miracle plant 
+	 * @param id - object id 
+	 * @param game - game object 
+	 * @param images - image of miracle plant
+	 */
 	public MiracleTree(double x, double y, ObjectId id, Game game, Images images) {
 		super(x, y, id, game);
 		hp=20;
 		this.images = images;
 	}
 	
+	/**
+	 * Method that sets timers 
+	 */
 	public void setTimer(){
 		timer0=System.currentTimeMillis();
 		timer1=System.currentTimeMillis();
 		timer2=System.currentTimeMillis();
 	}
 
+	/**
+	 * Method that drops compost and remove the miracle plant from the game
+	 */
 	public void dead(){
 //		dropSeed();
 		dropCompost();
 		game.handler.object.remove(this);
 		
 	}
+	/**
+	 * Method that drops compost 
+	 */
 	public void dropCompost() {
 	// TODO Auto-generated method stub
 		game.handler.addObject(new Compost(x, y, ObjectId.compost2, game, 1, images));
@@ -48,6 +67,14 @@ public class MiracleTree extends GameObject{
 //	}
 	
 
+	/**
+	 * Method that changes variables per call
+	 * 	- Add x and y velocity to x position to make the miracle plant move int the x and y position
+	 * 	- if time 0 is greater 5 seconds increment stage to show decay
+	 * 	- if timer1 is greater than 20 seconds remove miracle plant form the game
+	 * 	- if not in bounds timer reset
+	 * 	- if timer 2 greater than 15 seconds then drop compost  
+	 */
 	@Override
 	public void tick(ArrayList<GameObject> object) {
 		
@@ -76,6 +103,9 @@ public class MiracleTree extends GameObject{
 	}
 
 
+	/**
+	 * Method that displays the miracle plant image
+	 */
 	@Override
 	public void render(Graphics g) {
 		g.drawImage(images.getWaterStarGrass(stage),(int)x-16, (int)y+32, game);
@@ -85,6 +115,10 @@ public class MiracleTree extends GameObject{
 		g.drawString("Water Stargrass", (int) x - 24, (int) y+108); 
 	}
 
+	/**
+	 * Method that returns the bounds of the miracle plant
+	 * Used to tell if the plant collides with other objects 
+	 */
 	@Override
 	public Rectangle getBounds() {
 		return new Rectangle((int)x, (int)y, 32, 96);
