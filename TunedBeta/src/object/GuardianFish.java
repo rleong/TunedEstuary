56c99a5ca646ce14670b6bce27a472dd026c193d
Russell Leong <rleong@udel.edu>
2016-12-08 05:23:04 -0500

    Guardian Fish done

--- TunedBeta/src/object/GuardianFish.java
+++ TunedBeta/src/object/GuardianFish.java
@@ -13,24 +13,30 @@ import gfx.Images;
 import window.Handler;
 
 public class GuardianFish extends GameObject {
-	//Attributes
+	// Attributes
 	int damage;
 	Game game;
 	boolean chasing;
 	MiracleTree mt;
-	boolean firstTime=true;
+	boolean firstTime = true;
 	Images images;
 	int stage = 0;
 
 	/**
-	 * Constructor that constructs a guardian fish object. This object is used to attack the player 
-	 * if the get too close to the miracle plant to stop the boss.
+	 * Constructor that constructs a guardian fish object. This object is used
+	 * to attack the player if the get too close to the miracle plant to stop
+	 * the boss.
 	 * 
-	 * @param x - x position of guardian fish
-	 * @param y - y position of guardian fish
-	 * @param id - object id
-	 * @param game - game object
-	 * @param images - image of guardian fish
+	 * @param x
+	 *            - x position of guardian fish
+	 * @param y
+	 *            - y position of guardian fish
+	 * @param id
+	 *            - object id
+	 * @param game
+	 *            - game object
+	 * @param images
+	 *            - image of guardian fish
 	 */
 	public GuardianFish(double x, double y, ObjectId id, Game game, Images images) {
 		super(x, y, id, game);
@@ -40,33 +46,34 @@ public class GuardianFish extends GameObject {
 		velX = .5;
 		velY = .5;
 		this.images = images;
-		this.mt=new MiracleTree(game.dm.getWidth()*7/4, game.dm.getHeight()*6/5-192, ObjectId.MiracleTree, game, images);
+		this.mt = new MiracleTree(game.dm.getWidth() * 7 / 4, game.dm.getHeight() * 6 / 5 - 192, ObjectId.MiracleTree,
+				game, images);
 	}
 
 	/**
-	 * Method that changes variables per call
-	 * 	-if chasing is false: add x velocity and y velocity to x position and y position to move them in the x and y position and
-	 * 		set boundaries that the fish cannot pass
-	 * 	-call the collision function
+	 * Method that changes variables per call -if chasing is false: add x
+	 * velocity and y velocity to x position and y position to move them in the
+	 * x and y position and set boundaries that the fish cannot pass -call the
+	 * collision function
 	 */
 	@Override
 	public void tick(ArrayList<GameObject> object) {
-		if(!chasing){
-		x += velX;
-		y += velY;
-		if (x < (game.dm.getWidth() * 1.517) || x > (game.dm.getWidth() * 1.98)) {
-			velX *= -1;
+		if (!chasing) {
+			x += velX;
+			y += velY;
+			if (x < (game.dm.getWidth() * 1.517) || x > (game.dm.getWidth() * 1.98)) {
+				velX *= -1;
+			}
+			if (y < game.dm.getHeight() * 73 / 100 || y > game.dm.getHeight() * 1.07) {
+				velY *= -1;
+			}
 			if(velX > 0)
 				stage = 1;
 			else
 				stage = 0;
 		}
-		if (y < game.dm.getHeight() * 73 / 100 || y > game.dm.getHeight() * 1.07) {
-			velY *= -1;
-		}
-		}
 		collision(game.handler.object);
-		
+
 	}
 
 	/**
@@ -74,18 +81,19 @@ public class GuardianFish extends GameObject {
 	 */
 	@Override
 	public void render(Graphics g) {
-//		g.setColor(Color.RED);
-//		g.fillRect((int) game.dm.getWidth() * 1517 / 1000, (int) game.dm.getHeight() * 73 / 100, 960, 420);
-//		g.setColor(Color.green);
-//		g.fillRect((int) x, (int) y, 64, 32);
-		
+		// g.setColor(Color.RED);
+		// g.fillRect((int) game.dm.getWidth() * 1517 / 1000, (int)
+		// game.dm.getHeight() * 73 / 100, 960, 420);
+		// g.setColor(Color.green);
+		// g.fillRect((int) x, (int) y, 64, 32);
+
 		g.drawImage(images.getGuardianFish(stage), (int) x, (int) y, game);
-		
+
 	}
 
 	/**
-	 * Method that returns the bounds of the guardian fish.
-	 * Used to tell if the guardian fish is colling with another game object
+	 * Method that returns the bounds of the guardian fish. Used to tell if the
+	 * guardian fish is colling with another game object
 	 */
 	@Override
 	public Rectangle getBounds() {
@@ -93,8 +101,8 @@ public class GuardianFish extends GameObject {
 	}
 
 	/**
-	 * Method that returns the bounds the fish is guarding
-	 * Used to check if the critter is in the bounds so the fish can attack
+	 * Method that returns the bounds the fish is guarding Used to check if the
+	 * critter is in the bounds so the fish can attack
 	 * 
 	 * @return guard bounds
 	 */
@@ -104,47 +112,49 @@ public class GuardianFish extends GameObject {
 	}
 
 	/**
-	 * Method that checks if a certain object is colliding with the guardian fish and performs a certain action if so
-	 * 	-if critter colliding with guard bounds: Move towards the player
+	 * Method that checks if a certain object is colliding with the guardian
+	 * fish and performs a certain action if so -if critter colliding with guard
+	 * bounds: Move towards the player
+	 * 
 	 * @param object
 	 */
 	private void collision(ArrayList<GameObject> object) {
 		for (int i = 0; i < game.handler2.object.size(); i++) {
 			GameObject temp = game.handler2.object.get(i);
-			if (temp.getId()  == ObjectId.critter) {
+			if (temp.getId() == ObjectId.critter) {
 				if (getGuardBounds().intersects(temp.getBounds())) {
 					chasing = true;
-					mt.lock=true;
-					if(firstTime){
+					mt.lock = true;
+					if (firstTime) {
 						game.handler.addObject(mt);
 						mt.setTimer();
-						firstTime=false;
+						firstTime = false;
 					}
-					if(this.x <= temp.getX()){
+					if (this.x <= temp.getX()) {
 						velX = 1.2;
 						x += velX;
-						
+						stage = 1;
 					}
-					if(this.x >= temp.getX() && x > (game.dm.getWidth() * 1.517)){
+					if (this.x >= temp.getX() && x > (game.dm.getWidth() * 1.517)) {
 						velX = 1.2;
 						x -= velX;
-						
+						stage = 0;
 					}
-					if( this.y <= temp.getY()){
+					if (this.y <= temp.getY()) {
 						velY = 1.2;
 						y += velY;
-						
+
 					}
-					if( this.y >= temp.getY() && y > game.dm.getHeight() * 73 / 100 ){
+					if (this.y >= temp.getY() && y > game.dm.getHeight() * 73 / 100) {
 						velY = 1.5;
 						y -= velY;
-						
+
 					}
 				}
 				if (!getGuardBounds().intersects(temp.getBounds())) {
 					chasing = false;
-					mt.lock=false;
-					
+					mt.lock = false;
+
 				}
 			}
 		}
