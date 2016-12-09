47ad150637391c120a9756172c3ec218c4159786
jsaid <justins@udel.edu>
2016-12-09 18:30:35 -0500

    fdsf

---
+++ TunedBeta/src/tests/GuardianFishTest.java
@@ -0,0 +1,87 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import java.awt.Rectangle;
+
+import org.junit.Test;
+
+import framework.ObjectId;
+import object.GuardianFish;
+import object.LandSurface;
+import window.Window;
+
+public class GuardianFishTest {
+
+	GuardianFish tester = new GuardianFish(50.0,50.0,ObjectId.guardian,null, null);
+	
+	@Test
+	public void getGuardBoundsTest(){
+		assertEquals(tester.getGuardBounds(), new Rectangle(1984,480,640,800));
+	}
+	@Test
+	public void getBoundsTest(){
+		assertEquals(tester.getBounds(), new Rectangle(50,50,64,32));
+	}
+	@SuppressWarnings("deprecation")
+	@Test
+	public void getXTest() {
+		assertEquals(tester.getX(),01,50.0);
+	}
+	@Test
+	public void getYTest() {
+		assertEquals(tester.getY(),01,50.0);
+	}
+	@Test
+	public void setXTest() {
+		tester.setX(60.0);
+		assertEquals(tester.getX(),01,60.0);
+	}
+	@Test
+	public void setYTest() {
+		tester.setY(60.0);
+		assertEquals(tester.getY(),01,60.0);
+	}
+	@Test
+	public void getVelXTest() {
+		assertEquals(tester.getVelX(),01,1.0);
+	}
+	@Test
+	public void getVelYTest() {
+		assertEquals(tester.getVelY(),01,1.0);
+	}
+	@Test
+	public void setVelXTest() {
+		tester.setVelX(6.0);
+		assertEquals(tester.getVelX(),1,6.0);
+	}
+	@Test
+	public void setVelYTest() {
+		tester.setVelY(6.0);
+		assertEquals(tester.getVelY(),01,6.0);
+	}
+	@Test
+	public void getIdTest() {
+		assertEquals(tester.getId(),ObjectId.guardian);
+	}
+	
+	@Test
+	public void setGravityTest(){
+		tester.setGravity(65.0);
+		assertEquals(tester.getGravity(),01,65.0);
+	}
+	@Test
+	public void getGravityTest(){
+		assertEquals(tester.getGravity(),01,1);
+	}
+	@Test
+	public void setFallingTest(){
+		tester.setFalling(false);
+		assertEquals(tester.getFalling(), false);
+	}
+	@Test
+	public void getFallingTest(){
+		assertEquals(tester.getFalling(), true);
+	}
+
+}
